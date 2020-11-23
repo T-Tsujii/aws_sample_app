39947663f02dacd9c0ed3b2514766edb8244a4bf
T.Tsujii <ttakehisabt@gmail.com>
2020-11-23 23:29:23 +0900

    mysql -> psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_10_30_012310) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
